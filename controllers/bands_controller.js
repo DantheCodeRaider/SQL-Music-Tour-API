4aed22e7c2923bc46320c33a32d7e75d13e8df7f
Dan <dan@coderaider.tech>
2022-11-30 20:35:48 -0600

    Expanded controllers

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -1,13 +1,14 @@
 // DEPENDENCIES
 const bands = require('express').Router()
 const db = require('../models')
-const { Band } = db 
+const { Band, MeetGreet, SetTime, Event } = db 
 const { Op } = require('sequelize')
-   
+
 // FIND ALL BANDS
 bands.get('/', async (req, res) => {
     try {
         const foundBands = await Band.findAll({
+            limit: 10,
             order: [ [ 'available_start_time', 'ASC' ] ],
             where: {
                 name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` }
@@ -19,7 +20,43 @@ bands.get('/', async (req, res) => {
     }
 })
 
-
+// FIND A SPECIFIC BAND
+bands.get('/:name', async (req, res) => {
+    try {
+        const foundBand = await Band.findOne({
+            where: { name: req.params.name },
+            include: [
+                { 
+                    model: MeetGreet, 
+                    as: "meet_greets", 
+                    attributes: { exclude: ["band_id", "event_id"] },
+                    include: { 
+                        model: Event, 
+                        as: "event", 
+                        where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%` } } 
+                    }
+                },
+                { 
+                    model: SetTime, 
+                    as: "set_times",
+                    attributes: { exclude: ["band_id", "event_id"] },
+                    include: { 
+                        model: Event, 
+                        as: "event", 
+                        where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%` } } 
+                    }
+                }
+            ],
+            order: [
+                [{ model: MeetGreet, as: "meet_greets" }, { model: Event, as: "event" }, 'date', 'DESC'],
+                [{ model: SetTime, as: "set_times" }, { model: Event, as: "event" }, 'date', 'DESC']
+            ]
+        })
+        res.status(200).json(foundBand)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 // CREATE A BAND
 bands.post('/', async (req, res) => {
@@ -31,18 +68,6 @@ bands.post('/', async (req, res) => {
         })
     } catch(err) {
         res.status(500).json(err)
-    }
-})
-
-// FIND A SPECIFIC BAND
-bands.get('/:id', async (req, res) => {
-    try {
-        const foundBand = await Band.findOne({
-            where: { band_id: req.params.id }
-        })
-        res.status(200).json(foundBand)
-    } catch (error) {
-        res.status(500).json(error)
     }
 })
 
--- server.js
+++ server.js
@@ -2,31 +2,12 @@
 const express = require('express')
 const { Sequelize } = require('sequelize')
 const app = express()
+const bands = require('./controllers/bands_controller')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
-// SEQUELIZE CONNECTION
-/* const sequelize = new Sequelize(process.env.PG_URI)
-
-// SEQUELIZE CONNECTION
- const sequelize = new Sequelize({
-    storage: process.env.PG_URI,
-    dialect: 'postgres',
-    username: 'postgres',
-    password: 'my_password'
-  })
-
-try {
-    sequelize.authenticate() 
-    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
-} catch(err) {
-    console.log(`Unable to connect to PG: ${err}`) 
-} */
-
-
 
 // ROOT
 app.get('/', (req, res) => {
@@ -39,17 +20,13 @@ app.get('/', (req, res) => {
 const bandsController = require('./controllers/bands_controller')
 app.use('/bands', bandsController)
 
+const eventsController = require('./controllers/events_controller')
+app.use('/events', eventsController)
+
+const stagesController = require('./controllers/stages_controller')
+app.use('/stages', stagesController)
+
 // LISTEN
 app.listen(process.env.PORT, () => {
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
 })
-
-
-// SEQUELIZE CONNECTION
-/* const sequelize = new Sequelize({
-    storage: process.env.PG_URI,
-    dialect: 'postgres',
-    username: 'postgres',
-    password: 'my_password'
-  }) */
-  
